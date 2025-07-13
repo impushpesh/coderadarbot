767a0361fb3722eef7ca0e34e00f2a4bbd0f8de6
Pushpesh <ppant9418@gmail.com>
2025-07-13 23:25:09 +0530

    Configured cloud logging and switched to webhook from polling

--- bot.js
+++ bot.js
@@ -1,22 +1,64 @@
+// Polling
+// import { Telegraf } from "telegraf";
+// import logger from "./logger/logger.js"
+// import dotenv from "dotenv";
+// import connectDB from "./lib/connection.js";
+// import { registerCommands } from "./commands/index.js";
+
+
+// dotenv.config();
+
+// await connectDB(); 
+
+// const bot = new Telegraf(process.env.BOT_TOKEN);
+
+// registerCommands(bot);
+
+// // Bot launch
+// bot.launch();
+// logger.debug("Bot is running...")
+
+
+// bot.catch((err) => {
+//   logger.fatal("[SYSTEM] [BOT] An error occurred while running bot:", err);
+// });
+
+//---------------------Webhook---------------------
+import express from "express";
 import { Telegraf } from "telegraf";
-import logger from "./logger/logger.js"
+import logger from "./logger/logger.js";
 import dotenv from "dotenv";
 import connectDB from "./lib/connection.js";
 import { registerCommands } from "./commands/index.js";
 
-
 dotenv.config();
-
-await connectDB(); 
+await connectDB();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 registerCommands(bot);
 
-// Bot launch
-bot.launch();
-logger.debug("Bot is running...")
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+const secretPath = `/telegraf/${process.env.BOT_TOKEN}`;
+
+// Middleware to handle webhook updates from Telegram
+app.use(bot.webhookCallback(secretPath));
+
+
+// Start Express server
+app.listen(PORT, async () => {
+  try {
+    const webhookUrl = `${process.env.WEBHOOK_URL}${secretPath}`;
+    await bot.telegram.setWebhook(webhookUrl);
+
+    logger.info(`[WEBHOOK] Webhook set to: ${webhookUrl}`);
+    logger.info(`[SYSTEM] Server running on port ${PORT}`);
+  } catch (err) {
+    logger.error(`[WEBHOOK] Failed to set webhook: ${err.message}`);
+  }
+});
 
 bot.catch((err) => {
   logger.fatal("[SYSTEM] [BOT] An error occurred while running bot:", err);
--- logger/logger.js
+++ logger/logger.js
@@ -1,8 +1,21 @@
 // Main logger file
 import winston from "winston";
+import dotenv from "dotenv";
 
 import {loglevels, logcolors} from "./levels.js"
 import { transport } from "./transport.js";
+
+// For transporting logs to telemetary
+import { Logtail } from '@logtail/node';
+import { LogtailTransport } from '@logtail/winston';
+
+dotenv.config();
+
+const logtail = new Logtail(process.env.SOURCE_TOKEN, {
+
+  endpoint: `https://${process.env.INGESTING_HOST}`,
+
+});
 
 // Adding custom log colors to winston
 winston.addColors(logcolors);
@@ -11,7 +24,7 @@ winston.addColors(logcolors);
 const logger = winston.createLogger({
   levels: loglevels,
   level: process.env.LOG_LEVEL || "debug",
-  transports: transport
+  transports: [...transport,    new LogtailTransport(logtail)]
 });
 
 export default logger;
