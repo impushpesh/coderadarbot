64258437df44da63efab090b766f81b866c48340
Pushpesh <ppant9418@gmail.com>
2025-07-24 11:35:24 +0530

    Implemented rate limit

--- bot.js
+++ bot.js
@@ -26,6 +26,7 @@
 //---------------------Webhook---------------------
 import express from "express";
 import { Telegraf } from "telegraf";
+import rateLimit from "telegraf-ratelimit";
 import logger from "./logger/logger.js";
 import dotenv from "dotenv";
 import connectDB from "./lib/connection.js";
@@ -34,8 +35,20 @@ import { registerCommands } from "./commands/index.js";
 dotenv.config();
 await connectDB();
 
+// Rate limit config
+const limitConfig = {
+  window: 3000, // 3 seconds
+  limit: 1, // Max 1 message per 3 seconds
+  keyGenerator: (ctx) => ctx.from.id, // for specific user
+  onLimitExceeded: (ctx) => ctx.reply('Too many requests! Please slow down.')
+};
+
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+// Set up rate limiting middleware
+bot.use(rateLimit(limitConfig));
+
+// Register commands
 registerCommands(bot);
 
 const app = express();
